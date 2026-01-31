0b487bf840a831039b0bbfc15359b765188fa5b3
Rudolph Almeida <rudolf1.almeida@gmail.com>
2026-01-31 19:08:34 +1100

    Implement storage

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ use crate::ui::GbaApp;
 
 mod ui;
 
+const APP_ID: &'static str = "rgba";
+
 fn main() -> eframe::Result {
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        viewport: egui::ViewportBuilder::default().with_app_id(APP_ID).with_inner_size([960.0, 640.0]),
         ..Default::default()
     };
     eframe::run_native(
--- src/ui.rs
+++ src/ui.rs
@@ -3,10 +3,13 @@ use eframe::{egui, CreationContext, Frame};
 use gba::gba::Gba;
 use std::path::PathBuf;
 
+#[derive(Default, serde::Serialize, serde::Deserialize)]
 pub struct GbaApp {
     trace_opcode_viewer: TraceOpcodeViewer,
+    #[serde(skip)]
     gba: Option<Gba>,
 
+    #[serde(skip)]
     rom_path: Option<PathBuf>,
     bios_path: Option<PathBuf>,
 
@@ -14,7 +17,11 @@ pub struct GbaApp {
 }
 
 impl GbaApp {
-    pub fn new(_cc: &CreationContext) -> Self {
+    pub fn new(cc: &CreationContext) -> Self {
+        if let Some(storage) = cc.storage {
+            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        }
+
         Self {
             trace_opcode_viewer: TraceOpcodeViewer::new(),
             gba: None,
@@ -51,12 +58,17 @@ impl GbaApp {
                         self.begin_rom_if_possible();
                     }
                 }
+                ui.separator();
                 if ui.button("Select BIOS").clicked() {
                     if let Some(bios) = rfd::FileDialog::new().pick_file() {
                         self.bios_path = Some(bios.into());
                         self.begin_rom_if_possible();
                     }
                 }
+                if let Some(bios) = self.bios_path.as_ref() {
+                    ui.label(format!("{}", bios.to_str().unwrap()));
+                }
+                ui.separator();
                 if ui.button("Quit").clicked() {
                     ui.ctx().send_viewport_cmd(egui::ViewportCommand::Close);
                 }
@@ -69,8 +81,14 @@ impl eframe::App for GbaApp {
     fn update(&mut self, ctx: &Context, frame: &mut Frame) {
         self.render_ui(ctx, frame);
     }
+
+    fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        eframe::set_value(storage, eframe::APP_KEY, self);
+    }
+
 }
 
+#[derive(Debug, Default, serde::Deserialize, serde::Serialize)]
 struct TraceOpcodeViewer;
 
 impl TraceOpcodeViewer {
